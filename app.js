65448436680897c7aa6af2a507de2b4c98132032
hwan kim <rlaghks1103@gmail.com>
2021-11-14 21:21:36 +0900

    start

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const request = require('request');
+const path = require('path');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(
+    morgan('dev'),
+    express.json(),
+    express.urlencoded({ extended: false }),
+    cookieParser(process.env.SECRET),
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false
+        },
+        name: 'session-cookie'
+    })
+)
+
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+var ps = new kakao.maps.services.Places(); 
+
+ps.keywordSearch('이태원 맛집', placesSearchCB);
+
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/map.html"));
+});
+
+app.listen(app.get('port'), () => console.log(app.get('port'), '번 포트에서 대기 중'));
